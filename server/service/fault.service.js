8fdad84f5897c7714c835843c78e909eb2e23cf7
nguyenquan5895 <nguyenquan5895@gmail.com>
2021-03-26 17:22:54 +0700

    [Faults] Post fault

--- server/service/fault.service.js
+++ server/service/fault.service.js
@@ -1,15 +1,35 @@
 const { file, http } = require('../utils');
-const globalConfig = require('../config/global')
+const globalConfig = require('../config/global');
+const configService = require("./config.service");
 
 const fileName = 'fault.json';
+
+const createFault = async (faultData = {}) => {
+  const { MasterKey } = await configService.getConfigData();
+  const requestUrl = globalConfig.SERVER_API + '/deviceLogs'
+  const data = {
+    masterKey: MasterKey,
+    faultData
+  }
+  const res = await http.getPostWithConfig(requestUrl, data);
+  if (res && !res.code) {
+    console.log("Fault created");
+  } else {
+    console.log("Fault create fail");
+  }
+};
 
 const getFaultData = async () => {
   return await file.readJSONFile(fileName);
 };
 
 const saveFaultData = async (faultData = {}) => {
-  const res = await http.getAsyncWithConfig(globalConfig.SERVER_API);
-  // console.log(res)
+  if (faultData && faultData.list && faultData.list.length) {
+    await createFault(faultData.list[0])
+  } else {
+    console.log("Fault not found");
+  }
+
   faultData.updateAt = new Date();
   await file.writeJSONFile(fileName, faultData);
 };
