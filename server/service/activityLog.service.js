0dd343ddc829ffec24dfd12593515a5ab2cbf245
nguyenquan5895 <nguyenquan5895@gmail.com>
2021-03-26 16:59:06 +0700

    [ActivityLogs] Post activity log

--- server/service/activityLog.service.js
+++ server/service/activityLog.service.js
@@ -1,47 +1,69 @@
 const { file, http } = require('../utils');
 const globalConfig = require('../config/global');
 const { actionTypes, alertTypes } = require('../middlewares/master');
+const configService = require("./config.service");
 
 const fileName = 'activityLog.json';
+
+const createActivityLog = async (activityLogData = {}) => {
+  const { MasterKey } = await configService.getConfigData();
+  const requestUrl = globalConfig.SERVER_API + '/activityLogs'
+  const data = {
+    masterKey: MasterKey,
+    ...activityLogData
+  }
+  const res = await http.getPostWithConfig(requestUrl, data);
+  if (res && !res.code) {
+    console.log("ActivityLog created");
+  } else {
+    console.log("ActivityLog create fail");
+  }
+};
 
 const getActivityLogData = async () => {
   return await file.readJSONFile(fileName);
 };
 
 const saveActivityLogData = async (activityLogData = {}) => {
-  const res = await http.getAsyncWithConfig(globalConfig.SERVER_API);
-  // console.log(res)
   await file.writeJSONFile(fileName, activityLogData);
 };
 
-const pushActivityLog = async ({ category = '', type = '', description = '', details = {} } = {}) => {
+const pushActivityLog = async ({ category = '', type = '', description = '', activityLogData = {} } = {}) => {
   if (!category) {
     console.log('the activity log must has category field.')
     return;
   }
 
-  const activityLogData = await getActivityLogData();
-  if (!activityLogData.list) {
-    activityLogData.list = [];
-  }
-  activityLogData.service = actionTypes.ACTIVITY_LOG;
-  activityLogData.list = [{
+  const activityLog = {
     category,
+    type,
     description,
+    activityLogData,
+  }
+
+  await createActivityLog(activityLog);
+
+  const activityLogLocalData = await getActivityLogData();
+  if (!activityLogLocalData.list) {
+    activityLogLocalData.list = [];
+  }
+  activityLogLocalData.service = actionTypes.ACTIVITY_LOG;
+  activityLogLocalData.list = [{
+    ...activityLog,
     updateAt: new Date()
-  }, ...activityLogData.list]
+  }, ...activityLogLocalData.list]
 
-  await saveActivityLogData(activityLogData);
+  await saveActivityLogData(activityLogLocalData);
 }
 
-const success = async ({ category = '', type = '', description = '', details = {} } = {}) => {
-  await pushActivityLog({ category, description, details, type: alertTypes.SUCCESS })
+const success = async ({ category = '', type = '', description = '', activityLogData = {} } = {}) => {
+  await pushActivityLog({ category, description, activityLogData, type: alertTypes.SUCCESS })
 }
-const error = async ({ category = '', type = '', description = '', details = {} } = {}) => {
-  await pushActivityLog({ category, description, details, type: alertTypes.ERROR })
+const error = async ({ category = '', type = '', description = '', activityLogData = {} } = {}) => {
+  await pushActivityLog({ category, description, activityLogData, type: alertTypes.ERROR })
 }
-const warning = async ({ category = '', type = '', description = '', details = {} } = {}) => {
-  await pushActivityLog({ category, description, details, type: alertTypes.WARNING })
+const warning = async ({ category = '', type = '', description = '', activityLogData = {} } = {}) => {
+  await pushActivityLog({ category, description, activityLogData, type: alertTypes.WARNING })
 }
 
 module.exports = {
