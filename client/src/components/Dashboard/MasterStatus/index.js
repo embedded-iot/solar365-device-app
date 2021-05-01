4f6009178df40051bd301f074d4a8b0dda601be3
nguyenquan5895 <nguyenquan5895@gmail.com>
2021-05-01 17:45:51 +0700

    [Client] Update dashboard page

--- client/src/components/Dashboard/MasterStatus/index.js
+++ client/src/components/Dashboard/MasterStatus/index.js
@@ -3,20 +3,21 @@ import { Badge } from "antd";
 import moment from 'moment';
 
 import './style.scss';
+import { FormattedMessage } from "react-intl";
 
 
 const MasterStatus = (props) => (
   <div className="master-status__wrapper">
     <div className="master-status__item">
-      <span className="master-status__title">Trạng thái:</span>
+      <span className="master-status__title"><FormattedMessage id="STATUS" />:</span>
       <span className="master-status__value">
         {
-          props.isConnected ? <Badge color="#2db7f5" text="Kết nối" /> : <Badge color="#f50" text="Không kết nối" />
+          props.isConnected ? <Badge color="#2db7f5" text={<FormattedMessage id="CONNECTED" />} /> : <Badge color="#f50" text={<FormattedMessage id="NO_CONNECT" />} />
         }
       </span>
     </div>
     <div className="master-status__item">
-      <span className="master-status__title">Số thiết bị:</span>
+      <span className="master-status__title"><FormattedMessage id="TOTAL_DEVICES" />:</span>
       <span className="master-status__value">{props.deviceCount || '-'}</span>
     </div>
     <div className="master-status__item">
@@ -24,11 +25,11 @@ const MasterStatus = (props) => (
       <span className="master-status__value">{props.ip || '-'}</span>
     </div>
     <div className="master-status__item">
-      <span className="master-status__title">Chu kỳ đọc dữ liệu từ Logger:</span>
+      <span className="master-status__title"><FormattedMessage id="REQUEST_DATA_INTERVAL" />:</span>
       <span className="master-status__value">{`${props.clientConnectInterval || '-'} (s)`}</span>
     </div>
     <div className="master-status__item">
-      <span className="master-status__title">Thời gian cập nhập gần nhất:</span>
+      <span className="master-status__title"><FormattedMessage id="LAST_UPDATED_DATA_TIME" />:</span>
       <span className="master-status__value">{props.updateAt ? moment(props.updateAt).format('DD MM YYYY - HH:mm') : '-'}</span>
     </div>
   </div>
