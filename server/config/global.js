66b9c9de5a5429bd854cdd737550deb951d6d93b
nguyenquan5895 <nguyenquan5895@gmail.com>
2021-05-15 09:14:18 +0700

    Æ[Sáºerver] update solar365 ÆÆwebserver endpoint

--- server/config/global.js
+++ server/config/global.js
@@ -1,8 +1,8 @@
 const globalConfig = {
   vendor: ['SUNGROW POWER SUPPLY CO.,LTD.', 'Quectel Wireless Solutions Co., Ltd.'],
-  SERVER_API_Stg: 'https://unified-be.herokuapp.com/v1',
-  SERVER_API: 'http://connector.com.vn:8081/v1',
-  SERVER_API_1: 'http://localhost:3001/v1',
+  SERVER_API: 'https://unified-be.herokuapp.com/v1',
+  // SERVER_API: 'http://connector.com.vn:8081/v1',
+  // SERVER_API_1: 'http://localhost:3001/v1',
   CLIENT_CONNECT_INTERVAL: (1 * 60000) // 2 minutes
 }
 
